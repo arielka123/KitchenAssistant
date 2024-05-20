266282d0ad4b89465bb5dc890315e516d64462f2
Arleta Madej <arleta.madej@gmail.com>
2024-05-20 12:31:56 +0200

    init

---
+++ src/main/java/com/azet/KitchenAssistant/KitchenAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.azet.KitchenAssistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azet/KitchenAssistant/KitchenAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azet.KitchenAssistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
