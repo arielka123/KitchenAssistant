c15e0a63a1e0218386de1e2524ca603cb01cbe24
Arleta Madej <arleta.madej@gmail.com>
2024-05-29 12:34:31 +0200

    added new classes

---
+++ src/main/java/com/azet/KitchenAssistant/Entity/Category.java
@@ -0,0 +1,8 @@
+package com.azet.KitchenAssistant.Entity;
+
+public class Category {
+
+    private int id;
+
+    private String name;
+}
---
+++ src/main/java/com/azet/KitchenAssistant/Entity/Nutrients.java
@@ -0,0 +1,28 @@
+package com.azet.KitchenAssistant.Entity;
+
+public class Nutrients {
+
+    //data for 100g of products
+
+    private int id;
+
+    private int kcalEnergy;
+
+    private int gFat;
+
+    private int gSaturatedFat;
+
+    private int gCarbohydrates;
+
+    private int gProtein;
+
+    private int gFiber;
+
+    private String nutritionGrades;
+}
+
+
+//TODO
+//https://world.openfoodfacts.net/api/v2/product/3017624010701?fields=product_name,nutriments,nutrition_grades
+
+//https://openfoodfacts.github.io/openfoodfacts-server/api/tutorial-off-api/#authentication
