12ee089c0bb3e0a3b7750ec9e6abf2e21b1b81b4
Arleta Madej <arleta.madej@gmail.com>
2024-06-03 13:34:54 +0200

    created tables products and categories in db

--- src/main/resources/db/migration/V2__init_products_table.sql
+++ src/main/resources/db/migration/V2__init_products_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS `kitchen_assistant`.`products`(
     id int primary key auto_increment,
     name varchar(100) not null,
     image varchar(255),
+    code_bar varchar(13),
     category_id int not null,
     foreign key (category_id) references categories (id)
 )
