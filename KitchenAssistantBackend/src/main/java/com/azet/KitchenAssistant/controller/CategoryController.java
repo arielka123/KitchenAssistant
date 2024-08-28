8516a055a7a2a7b61e5b91de46e6ac56ae9cc122
Arleta Madej <arleta.madej@gmail.com>
2024-08-28 17:59:16 +0200

    list of products is displayed

--- KitchenAssistantBackend/src/main/java/com/azet/KitchenAssistant/controller/CategoryController.java
+++ KitchenAssistantBackend/src/main/java/com/azet/KitchenAssistant/controller/CategoryController.java
@@ -13,6 +13,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/categories")
 class CategoryController {
--- KitchenAssistantBackend/src/main/java/com/azet/KitchenAssistant/controller/ProductController.java
+++ KitchenAssistantBackend/src/main/java/com/azet/KitchenAssistant/controller/ProductController.java
@@ -16,6 +16,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/products")
 class ProductController {
