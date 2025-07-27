14a85fb03e164b25a54633c3ff33796f6a2830be
Arleta Madej <arleta.madej@gmail.com>
2025-07-27 21:19:10 +0200

    first view

--- KitchenAssistantFrontend/src/app/common/product.ts
+++ KitchenAssistantFrontend/src/app/common/product.ts
@@ -1,7 +1,9 @@
 import { Category } from "./category";
 
 export class Product {
-    constructor(public name: string,
+    constructor(
+                public id: number,
+                public name: string,
                 public image: string,
                 public codeBar: string,
                 public category: Category
